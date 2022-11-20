efd117d75467a3c3a71c7aa2cfce61e28d0829cd
AlgoBloom <bloomblockchain@gmail.com>
2022-11-19 21:51:01 -0500

    Changed naming of the global byteslices for the voting option descriptions.

--- dao_contract.py
+++ dao_contract.py
@@ -7,13 +7,13 @@ on_creation = Seq(
     # Global byteslice 1
     App.globalPut(Bytes("App_Name"), Bytes("Community 1 Voting Application")),
     # Global byteslice 2
-    App.globalPut(Bytes("Option A"), Bytes("Details for option A.")),
+    App.globalPut(Bytes("A_Description"), Bytes("Details for option A.")),
     # Global byteslice 3
-    App.globalPut(Bytes("Option B"), Bytes("Details for option B.")),
+    App.globalPut(Bytes("B_Description"), Bytes("Details for option B.")),
     # Global byteslice 4
-    App.globalPut(Bytes("Option C"), Bytes("Details for option C.")),
+    App.globalPut(Bytes("C_Description"), Bytes("Details for option C.")),
     # Global byteslice 5
-    App.globalPut(Bytes("Option D"), Bytes("Details for option D.")),
+    App.globalPut(Bytes("D_Description"), Bytes("Details for option D.")),
     # Global int 1
     App.globalPut(Bytes("A_Count"), Int(0)),
     
