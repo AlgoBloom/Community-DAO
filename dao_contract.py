457d9b21416fa3528e4fd01b5374981c1f8de3be
AlgoBloom <bloomblockchain@gmail.com>
2022-11-19 21:55:20 -0500

    Added global integers to keep track of the count of different options.

--- dao_contract.py
+++ dao_contract.py
@@ -16,5 +16,12 @@ on_creation = Seq(
     App.globalPut(Bytes("D_Description"), Bytes("Details for option D.")),
     # Global int 1
     App.globalPut(Bytes("A_Count"), Int(0)),
-    
+    # Global int 2
+    App.globalPut(Bytes("B_Count"), Int(0)),
+    # Global int 3
+    App.globalPut(Bytes("C_Count"), Int(0)),
+    # Global int 4
+    App.globalPut(Bytes("D_Count"), Int(0)),
+    # Approves sequence
+    Return(Int(1))
 )
